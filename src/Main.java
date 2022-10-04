3d9a0767d3ed2a74f3ef829315c4ad59005d5ba5
Laura Conde <laurusmayer@gmail.com>
2022-10-04 13:37:11 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] codes = {"p1", "p2", "p3"}; //3 caracteres (6 bytes)
+        String[] descricion = {"parafusos", "cravos", "tachas"}; //10 caracteres (20 bytes)
+        int[] prices = {3,4,5}; //4 bytes
+
+
+
+    }
+}
